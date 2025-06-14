738b0c3ed58fc97b039c16bb9adbb90c94339dbd
5-6-1 <yanhongyi666@outlook.com>
2025-06-14 17:35:36 +0800

    add si support

--- src/unitx.nim
+++ src/unitx.nim
@@ -1,12 +1,14 @@
 from macros import quote,
   newIdentNode,kind,nnkTableConstr,items,
   expectKind,nnkExprColonExpr,`[]`,nnkInfix,expectIdent,
-  newCall,ident,toStrLit,newTree,nnkCall
-from strutils import split,replace,join,parseInt
+  newCall,ident,toStrLit,newTree,nnkCall,nnkStmtList,
+  nnkStaticStmt,newStmtList,`$`,error,add,nnkAsgn,
+  nnkBracketExpr,nnkTupleConstr
+from tables import `[]`,`[]=`,initTable,toTable,contains
+from strutils import split,replace,join,parseInt,contains
 from algorithm import sort
 from math import `^`,floor
-from tables import toTable,`[]`
-
+export tables.`[]=`
 
 
 const superScriptMap={'0':"⁰",'1':"¹",'2':"²",'3':"³",'4':"⁴",'5':"⁵",'6':"⁶",'7':"⁷",'8':"⁸",'9':"⁹"}.toTable
@@ -33,6 +35,87 @@ func readUnit*(u:static[string]):static[string]{.compileTime.}=
         result.add c
         flag=false
       d=0
+const siList = ["meter","kilogram","second","ampere","kelvin","mole","candela",""]
+var siTable*{.compileTime.}=initTable[string,(float,string)]()
+
+
+
+func cmpTupUnit(a,b:(string,(int,int))):int=a[0].cmp b[0]
+template tupUnitTemp(x)=
+  block:
+    let u=x
+    var ans=newseq[(string,(int,int))]()
+    let
+      list=u.split"//"
+      left=list[0].split'*'
+      right=if list.len>1:list[1].split'*' else:newSeq[string]()
+    for l in left:
+      let
+        llist=l.split"^"
+        base=llist[0]
+        exp=
+          if llist.len>1:
+            let nlist=llist[1].split"/"
+            let x=nlist[0].parseInt
+            if x==0: continue
+            if nlist.len==1:
+              (x,1)
+            else:
+              let y=nlist[1].parseInt
+              if y>0: simplifyFrac (x,y)
+              elif y<0: simplifyFrac (-x,-y)
+              else: raise newException(ValueError, "invalid exponent")
+          else:(1,1)
+      ans.add (base,exp)
+    for l in right:
+        let
+          llist=l.split"^"
+          base=llist[0]
+          exp=
+            if llist.len>1:
+              let nlist=llist[1].split"/"
+              let x=nlist[0].parseInt
+              if x==0: continue
+              if nlist.len==1:
+                (-x,1)
+              else:
+                let y=nlist[1].parseInt
+                if y>0:simplifyFrac (-x,y)
+                elif y<0:simplifyFrac (x,-y)
+                else: raise newException(ValueError, "invalid exponent")
+            else:(-1,1)
+        ans.add (base,exp)
+    ans.sort cmpTupUnit
+    ans
+template convertUnitHelpTemp(x,a)=
+  block:
+    let tup=tupUnitTemp(x)
+    var ans=(0,1)
+    for (s,e) in tup:
+      if s==a:
+        ans=e
+    ans
+template tupToUnitTemp(tups)=
+  block:
+    var
+      st=newSeq[string]()
+      stDiv=newSeq[string]()
+    for (s,e)in tups:
+      if e[0]>0:st.add s&(if e[0]!=1 or e[1]!=1: "^" & $e[0] & (if e[1]!=1:"/" & $e[1]else:"") else: "")
+      elif e[0]==0:continue
+      else:stDiv.add s&(if e[0] != -1 or e[1]!=1: "^" & $(-e[0]) & (if e[1]!=1:"/" & $e[1]else:"") else: "")
+    st.join("*")&(if stDiv.len>0:"//"&stDiv.join"*" else:"")
+template isSimpleSiUnittemp(s)=
+  block:
+    var ans=true
+    let tup=tupUnitTemp s
+    for (a,b) in tup:
+      if not (a in siList):
+        ans=false
+        break
+    ans
+
+
 
 func gcd(a,b: int): int {.compileTime.}=
   var (x, y) = (abs a, abs b)
@@ -100,6 +183,8 @@ func floatToFraction(x: float): (int, int) {.compileTime.}=
     if r < 1e-12:
       return (sign * h2.int, k2.int)
 
+
+
 func findInsertIndex(tups: seq[(string, (int, int))], target: string): int {.compileTime.} =
   var
     low = 0
@@ -119,7 +204,7 @@ func addInUnit(tups: var seq[(string, (int, int))], tup: (string, (int, int))) {
   let idx = tups.findInsertIndex(tup[0])
   if idx < tups.len and tups[idx][0] == tup[0]:
     let newExp = tups[idx][1].fracAdd(tup[1])
-    if newExp[0] == 0 or newExp[0]/newExp[1]<1e-9:
+    if newExp[0] == 0 or (newExp[0]/newExp[1]).abs<1e-9:
       tups.del(idx)
     else:
       tups[idx] = (tup[0], newExp)
@@ -131,8 +216,15 @@ func delOutUnit(tups: var seq[(string, (int, int))], target: string) {.compileTi
     tups.del(idx)
 
 
+
+
 type Unit*[T;U:static[string]]=distinct T #轻量单位类型,U为单位
-func `$`*(arg:Unit):string{.inline.} = $arg.T(arg)&" "&arg.U.readUnit
+func `$`*(arg:Unit):string{.inline.} =
+  when arg.U=="":
+    $arg.T(arg)
+  else:
+    $arg.T(arg)&" "&arg.U.readUnit
+converter withNoUnit[T](x:Unit[T,""]):T=T(x)
 func formatUnitHelper(u:static[string]):static[string] {.compileTime.}=
   let li=u.replace(" ","")
   var
@@ -156,8 +248,8 @@ func formatUnitHelper(u:static[string]):static[string] {.compileTime.}=
 func tupUnit(u:static[string]):static[seq[(string,(int,int))]]{.compileTime.}=
   let
     list=u.split"//"
-    left=list[0].split'*'
-    right=if list.len>1:list[1].split'*' else:newSeq[string]()
+    left=if u.contains"//"and list.len==1:newSeq[string]() else:list[0].split'*'
+    right=if list.len>1:list[1].split'*' elif u.contains"//":list[0].split'*' else:newSeq[string]()
   for l in left:
     let
       llist=l.split"^"
@@ -194,8 +286,7 @@ func tupUnit(u:static[string]):static[seq[(string,(int,int))]]{.compileTime.}=
             else: raise newException(ValueError, "invalid exponent")
         else:(-1,1)
     result.add (base,exp)
-  func comTupUnit(a,b:(string,(int,int))):int=cmp a[0],b[0]
-  result.sort comTupUnit
+  result.sort cmpTupUnit
 func tupToUnit(tups:static[seq[(string,(int,int))]]):static[string] {.compileTime.}=
   var
     st=newSeq[string]()
@@ -306,8 +397,80 @@ macro convertUnit*(val,conv):untyped =
       result = newCall(ident"convertUnitInner",result,toStrLit con[0],newTree(nnkCall,ident"formatUnit",toStrLit con[1][2]),con[1][1])#单位安全转换
 
 
-func `+`*[T;U:static[string]](l,r:Unit[T,U]):Unit[T,U]{.inline.} = Unit[T,U](T(l)+T(r))
-func `-`*[T;U:static[string]](l,r:Unit[T,U]):Unit[T,U]{.inline.} = Unit[T,U](T(l)-T(r))
+
+macro addSiUnit*(conv):untyped =
+  result=newTree(nnkStaticStmt,newStmtList())
+  if conv.kind==nnkTableConstr:
+    for con in conv:
+      expectKind con, nnkExprColonExpr
+      expectKind con[1],nnkInfix
+      expectIdent con[1][0],"~"
+      if $con[0] in siList:
+        error "can not change si"
+      result[0].add newTree(nnkAsgn,newTree(nnkBracketExpr,ident"siTable",toStrLit(con[0])),newTree(nnkTupleConstr,con[1][1],toStrLit con[1][2]))
+  else:
+    error "syntax error"
+proc toSimpleSiUnit(s:static[string]):static[(float,seq[(string,(int,int))])]{.compileTime.}=
+  var
+    tup=tupUnit s
+    num=1.0
+    sss=s
+    flag=true
+  while flag:
+    flag=false
+    for (a,b) in tup:
+      if a in siTable:
+        flag=true
+        let x=convertUnitHelpTemp(sss,a)
+        num*=siTable[a][0]^(x[0]/x[1])
+        tup=block:
+          let
+            val=sss
+            orign=a
+            to=siTable[a][1]
+          var tup=tupUnitTemp(val)
+          let toer=tupUnitTemp(to)
+          let e=convertUnitHelpTemp(val,orign)
+          delOutUnit(tup,orign)
+          for t in toer:
+            addInUnit(tup,(t[0],t[1].fracMul e))
+          tup
+        sss=tupToUnitTemp tup
+        break
+  let temp1=tupToUnitTemp tup
+  let jud=isSimpleSiUnittemp temp1
+  if jud:(num,tup)
+  else:error "not si unit"
+func convertSimpleSiUnitHelp(s:static[string]):static[string]=tupToUnit toSimpleSiUnit(s)[1]
+func convertSimpleSiUnit*[T;U:static[string]](s:Unit[T,U]):Unit[T,convertSimpleSiUnitHelp(U)]{.inline.}=
+  const t=U.toSimpleSiUnit
+  Unit[T,tupToUnit t[1]](s.float*t[0])
+
+
+
+
+func `+`*[T;U1,U2:static[string]](l:Unit[T,U1],r:Unit[T,U2]):Unit[T,U1]{.inline.} =
+  when U1==U2:
+    Unit[T,U1](T(l)+T(r))
+  else:
+    const
+      lsi=toSimpleSiUnit(U1)
+      rsi=toSimpleSiUnit(U2)
+      lsistr=tuptoUnit(lsi[1])
+      rsistr=tuptoUnit(rsi[1])
+    when lsistr!=rsistr:error "not same si unit"
+    Unit[T,U1]((lsi[0]*l.float+rsi[0]*r.float)/lsi[0])
+func `-`*[T;U1,U2:static[string]](l:Unit[T,U1],r:Unit[T,U2]):Unit[T,U1]{.inline.} =
+  when U1==U2:
+    Unit[T,U1](T(l)-T(r))
+  else:
+    const
+      lsi=toSimpleSiUnit(U1)
+      rsi=toSimpleSiUnit(U2)
+      lsistr=tuptoUnit(lsi[1])
+      rsistr=tuptoUnit(rsi[1])
+    when lsistr!=rsistr:error "not same si unit"
+    Unit[T,U1]((lsi[0]*l.float-rsi[0]*r.float)/lsi[0])
 func `*`*[T;U1,U2:static[string]](l:Unit[T,U1],r:Unit[T,U2]):
   Unit[T,mulUnit(U1,U2)]{.inline.}=Unit[T,mulUnit(U1,U2)](T(l)*T(r))
 func `/`*[T;U1,U2:static[string]](l:Unit[T,U1],r:Unit[T,U2]):
@@ -337,4 +500,18 @@ when isMainModule:
   let 时间=电脑/生产力
   let 日时间=时间.convertUnit {月:30.0~日}
   echo 日时间
+  addSiUnit {
+    m:1.0~meter,
+    kg:1.0~kilogram,
+    s:1.0~second
+  }
+
+  let a=1~m/s
+  let b=3~meter/second
+  echo a+b
+  echo (a+b).convertSimpleSiUnit
+
+  echo a.U
+  echo a.U.toSimpleSiUnit[1].tuptoUnit.readUnit
+  echo a.U.toSimpleSiUnit[0]
 
